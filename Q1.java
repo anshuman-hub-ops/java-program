63cd2d18a4ac9d75f4c30f26afdabb9011ad3afa
anshuman-hub-ops <aansuman514@gmail.com>
2025-09-09 20:25:37 +0530

    q1 completed

---
+++ Q1.java
@@ -0,0 +1,8 @@
+class Q1{
+ public static void main(String[] args) {
+        System.out.println("Name: Anshuman Ayush Baral");
+        System.out.println("Branch: CSE");
+        System.out.println("Roll No: 34");
+        System.out.println("College: Silicon University");
+    }
+}
